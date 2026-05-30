fe06e2c95de230c6279030b9a8c1426bad8338e6
cxy-yao <cxy20031219@gmail.com>
2026-05-30 12:07:28 +0800

    fix: 403 page only show '403'

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,11 +1,12 @@
 export async function onRequest(context) {
   const host = new URL(context.request.url).hostname;
   
-  // Allow chulinchen.top and www.chulinchen.top
   if (host === 'chulinchen.top' || host === 'www.chulinchen.top') {
     return context.next();
   }
   
-  // Block everything else (pages.dev etc)
-  return new Response(null, { status: 403 });
+  return new Response("403", {
+    status: 403,
+    headers: { "Content-Type": "text/plain" }
+  });
 }
